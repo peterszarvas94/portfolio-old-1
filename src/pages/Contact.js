117a5fa0cc842c062ee0dbfc41fe0b986b8c763a
Szarvas Péter <peterszarvas94@gmail.com>
2021-06-03 16:31:36 +0200

    adding animation and input check to contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
-// import emailjs, { init } from 'emailjs-com';
+
+//eslint-disable-next-line
+import emailjs, { init } from 'emailjs-com';
 import { FiMail, FiPhone } from 'react-icons/fi';
 import { FaPaperPlane, FaLinkedin } from 'react-icons/fa';
 
 import '../styles/Contact.css';
 
-// init("user_MWRHD7DbwWh1M82xabdWy");
+init("user_MWRHD7DbwWh1M82xabdWy");
 
 const Contact = () => {
 
+    // save data to send
     const [toSend, setToSend] = useState({
         contact_name: '',
         contact_email: '',
@@ -16,27 +19,90 @@ const Contact = () => {
         contact_message: ''
     }); 
 
+    //handle typing
     const handleChange = (e) => {
         setToSend({ ...toSend, [e.target.name]: e.target.value });
+        e.target.style.boxShadow = 'none'
+        let name = e.target.name.split('_')[1];
+        name = name.split('').map((e) => (name.indexOf(e) === 0) ? e.toUpperCase() : e).join('');
+        e.target.placeholder = name;
     };
 
+    //handle submiting
     const submitForm = async (e) => {
         e.preventDefault();
         e.stopPropagation();
 
+        const fields = document.getElementById('contact_form').querySelectorAll("[required]");
+
+        let error = false;
+
+        for (let i = 0; i < fields.length; i++) {
+
+            if(!fields[i].value) {
+                let name = fields[i].name.split('_')[1];
+                fields[i].placeholder = 'Enter your ' + name + ' there';
+                fields[i].style.boxShadow = 'inset 0 0 2px 2px red';
+                error = true;
+            }
+
+            if (i === 1) {
+                const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if(!re.test(fields[i].value)) {
+                    fields[i].value = '';
+                    fields[i].placeholder = 'Enter a valid email address';
+                    fields[i].style.boxShadow = 'inset 0 0 2px 2px red';
+                    error = true;
+                }
+            }
+        }
+
+        if (error) {
+            return;
+        }
+
         try {
-            // const response = await emailjs.send("service_tymk94v", "template_6od5ifg", {
-            //     contact_name: toSend.contact_name,
-            //     contact_subject: toSend.contact_subject,
-            //     contact_email: toSend.contact_email,
-            //     contact_message: toSend.contact_message
-            // });
-            // console.log(response);
+            const response = await emailjs.send("service_tymk94v", "template_6od5ifg", {
+                contact_name: toSend.contact_name,
+                contact_subject: toSend.contact_subject,
+                contact_email: toSend.contact_email,
+                contact_message: toSend.contact_message
+            });
+            console.log(response);
 
         } catch(err) {
             console.log(err);
         }
     }
+
+    //scroll animations
+    const handleContactScroll = () => {
+        const form_children = document.getElementById('contact_form').children;
+        const contact_element_top = document.getElementById('contact').getBoundingClientRect().top + window.pageYOffset;
+        const nav_height = document.getElementById('nav_list').getBoundingClientRect().height;
+
+        const addAminationClass = (i) => {
+            if (i+1 === form_children.length) {
+                form_children[i].classList.add('details_animation');
+            } else {
+                form_children[i].classList.add('contact_animation');
+            }
+            i++;
+            if (i < form_children.length) {
+                setTimeout(() => {
+                    addAminationClass(i);
+                }, 100);
+            } else {
+                document.removeEventListener('scroll', handleContactScroll);
+            }
+        }
+        
+        if (window.pageYOffset >= contact_element_top - nav_height - window.innerHeight*4/5) {
+           addAminationClass(0);
+        } 
+    }
+
+    document.addEventListener('scroll', handleContactScroll);
 
     return (
         <>
@@ -45,10 +111,9 @@ const Contact = () => {
 
                 {/* contact title */}
                 <h3 className='contact_title'>Contact me!</h3>
-
                 
                 {/* the form */}
-                <form className='contact_form' onSubmit={submitForm}>
+                <form id='contact_form' className='contact_form' onSubmit={submitForm}>
                     
                     {/* sender name */}
                     <input
@@ -110,16 +175,17 @@ const Contact = () => {
                         Send message&nbsp;<span className='contact_button_icon'><FaPaperPlane /></span> 
                     </button>
                     
-                    {/* details */}
+                    {/* my contact details */}
                     <div className='contact_details'>
                         <h3>Get in touch!</h3>
                         <ul>
+                            {/* Linkedin */}
                             <li>
                                 <span>
                                     <FaLinkedin />
                                 </span>
                                 <span>
-                                    <strong>Linkedin:</strong>
+                                    <strong>LinkedIn:</strong>
                                     <div>
                                         <a href='https://www.linkedin.com/in/peterszarvas94' target='_blank' rel="noopener noreferrer">
                                             linkedin.com/in/peterszarvas94
@@ -127,6 +193,7 @@ const Contact = () => {
                                     </div>
                                 </span>
                             </li>
+                            {/* Email */}
                             <li>
                                 <span>
                                     <FiMail />
@@ -140,6 +207,7 @@ const Contact = () => {
                                     </div>
                                 </span>
                             </li>
+                            {/* Phone */}
                             <li>
                                 <span>
                                     <FiPhone />
